59b801457158962b3ddfb98f3cd32744f3d8efe9
Kate Spitalnic <kespitalnic@gmail.com>
2022-05-29 21:52:25 -0400

    redux and top nav

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,23 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './booking/Home';
 import Login from './auth/Login';
 import Register from './auth/Register'; 
+import { Link } from 'react-router-dom'; 
+
+
+// create navigation 
+const TopNav = () => (
+  <div className="nav bg-light d-flex justify-content-between">
+    <Link className="nav-link" to="/">Home</Link>
+    <Link className="nav-link" to="/login">Login</Link>
+    <Link className="nav-link" to="/register">Register</Link>
+  </div>
+)
+
 
 function App() {
   return (
     <BrowserRouter>
+    <TopNav />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
