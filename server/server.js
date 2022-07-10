a79e45e7a75a213bb473093f023d5336fa7f06a0
Kate Spitalnic <kespitalnic@gmail.com>
2022-07-09 23:07:25 -0400

    server conotrollers and begin registration form

--- server/server.js
+++ server/server.js
@@ -1,9 +1,30 @@
-const express = require("express");
+import express from "express"; 
+import { readdirSync } from 'fs';
+import cors from "cors";
+import mongoose from "mongoose";
+require('dotenv').config(); 
+const morgan = require("morgan");
 
-const app = express()
+const app = express();
 
-app.get('/api/:message', (req, res) => {
-    res.status(200).send(`Here is your message: ${req.params.message}`);
-}); 
+// db connection 
+mongoose
+  .connect(process.env.DATABASE, {
+      useNewUrlParser: true,
+      useFindAndModify: false, 
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+  })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB Error => ", err));
 
-app.listen(8000, () => console.log('Server is running on port 8000'));
+// middlewares 
+app.use(cors());
+app.use(morgan("dev")); 
+
+// route middleware 
+readdirSync("./routes").map((r) => app.use('/api', require(`./routes/${r}`))); 
+
+const port = process.env.PORT || 8000;
+
+app.listen(8000, () => console.log(`Server is running on port ${port}`));
